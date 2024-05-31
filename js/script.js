dbe40f16271f7091cfade2352a0703eef303c64c
Santosh kumar gupta <sainkee1997@gmail.com>
2024-05-31 12:52:43 +0530

    btn added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuButton = document.querySelector('.openmenu');
-const closeButton = document.querySelector('.closemenu');
-var modal = document.getElementById('id01');
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuButton = document.querySelector(".openmenu");
+const closeButton = document.querySelector(".closemenu");
+const signOutButton = document.querySelector(".signOut");
+const signInButton = document.querySelector(".signin");
+const bookBtn = document.querySelector(".bookbtn");
+const usernameInput = document.querySelector('input[name="uname"]');
+const passwordInput = document.querySelector('input[name="psw"]');
+var modal = document.getElementById("id01");
 
-menuButton.addEventListener('click', () => {
-  mobileMenu.classList.add('active');
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
   document.body.style.overflow = "hidden";
 });
 
-closeButton.addEventListener('click', () => {
-  mobileMenu.classList.remove('active');
+closeButton.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
   document.body.style.overflow = "visible";
 });
 
@@ -18,4 +23,47 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+};
+
+window.onload = function () {
+  var userName = localStorage.getItem("userName");
+  var passWord = localStorage.getItem("passWord");
+  if (userName && passWord) {
+    signInButton.style.display = "none";
+    signOutButton.style.display = "block";
+    bookBtn.style.display = "block";
+  }
+};
+
+function loginHandler() {
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // Store data in localStorage if remember is checked
+
+  localStorage.setItem("userName", username);
+  localStorage.setItem("passWord", password);
+
+  // Redirect to the booking page
+  alert(`welcome ${username}`);
 }
+
+function signOutHandler() {
+  localStorage.removeItem("userName");
+  localStorage.removeItem("passWord");
+  signInButton.style.display = "block";
+  signOutButton.style.display = "none";
+  bookBtn.style.display = "none";
+  window.location.href = "index.html";
+}
+
+const bookTicketHandler = () => {
+  var userName = localStorage.getItem("userName");
+  var passWord = localStorage.getItem("passWord");
+  if (userName && passWord) {
+    window.location.href = "/BookingPage/index.html";
+    return;
+  }
+  alert("Please Login first!");
+};
